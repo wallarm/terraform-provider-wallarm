2dac4c8b79ff07c59cebd1fd70c50285e5348c2c
vkhatib-wallarm <vkhatib@wallarm.com>
2025-08-13 16:32:06 +0400

    PLUTO-8020: add mode to read

--- wallarm/common/resourcerule/resource_rule.go
+++ wallarm/common/resourcerule/resource_rule.go
@@ -61,6 +61,7 @@ func ResourceRuleWallarmRead(d *schema.ResourceData, clientID int, cli wallarm.A
 	d.Set("set", updatedRule.Set)
 	d.Set("threshold", apitotf.Threshold(updatedRule.Threshold))
 	d.Set("reaction", apitotf.Reaction(updatedRule.Reaction))
+	d.Set("mode", updatedRule.Mode)
 	d.Set("enumerated_parameters", apitotf.EnumeratedParameters(updatedRule.EnumeratedParameters))
 	d.Set("advanced_conditions", apitotf.AdvancedConditions(updatedRule.AdvancedConditions))
 	d.Set("arbitrary_conditions", apitotf.ArbitraryConditions(updatedRule.ArbitraryConditions))
