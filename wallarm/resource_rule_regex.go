2341149b0704199600aca5d534ae86b486a209a0
vkhatib-wallarm <vkhatib@wallarm.com>
2026-01-27 15:40:27 +0400

    PLUTO-8591: remove old code

--- wallarm/resource_rule_regex.go
+++ wallarm/resource_rule_regex.go
@@ -2,7 +2,6 @@ package wallarm
 
 import (
 	"fmt"
-	"log"
 	"strconv"
 	"strings"
 
@@ -10,7 +9,6 @@ import (
 	"github.com/wallarm/terraform-provider-wallarm/wallarm/common/resourcerule"
 	"github.com/wallarm/wallarm-go"
 
-	"github.com/google/go-cmp/cmp"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
 
@@ -124,124 +122,6 @@ func resourceWallarmRegexCreate(d *schema.ResourceData, m interface{}) error {
 
 func resourceWallarmRegexRead(d *schema.ResourceData, m interface{}) error {
 	return resourcerule.ResourceRuleWallarmRead(d, retrieveClientID(d), m.(wallarm.API))
-	client := m.(wallarm.API)
-	clientID := retrieveClientID(d)
-	actionID := d.Get("action_id").(int)
-	ruleID := d.Get("rule_id").(int)
-	regex := d.Get("regex").(string)
-	regexID := d.Get("regex_id").(float64)
-	attackType := d.Get("attack_type").(string)
-
-	experimental := d.Get("experimental").(bool)
-	var actionType string
-	if experimental {
-		actionType = "experimental_regex"
-	} else {
-		actionType = "regex"
-	}
-
-	var ps []interface{}
-	if v, ok := d.GetOk("point"); ok {
-		ps = v.([]interface{})
-	} else {
-		return nil
-	}
-	var points []interface{}
-	for _, point := range ps {
-		p := point.([]interface{})
-		points = append(points, p...)
-	}
-
-	actionsFromState := d.Get("action").(*schema.Set)
-	action, err := resourcerule.ExpandSetToActionDetailsList(actionsFromState)
-	if err != nil {
-		return err
-	}
-
-	actsSlice := make([]interface{}, 0, len(action))
-	for _, a := range action {
-		acts, err := actionDetailsToMap(a)
-		if err != nil {
-			return err
-		}
-		actsSlice = append(actsSlice, acts)
-	}
-
-	actionsSet := schema.NewSet(hashResponseActionDetails, actsSlice)
-
-	hint := &wallarm.HintRead{
-		Limit:     1000,
-		Offset:    0,
-		OrderBy:   "updated_at",
-		OrderDesc: true,
-		Filter: &wallarm.HintFilter{
-			Clientid: []int{clientID},
-			ID:       []int{ruleID},
-		},
-	}
-	actionHints, err := client.HintRead(hint)
-	if err != nil {
-		return err
-	}
-
-	// This is mandatory to fill in the default values in order to compare them deeply.
-	// Assign new values to the old struct slice.
-	fillInDefaultValues(&action)
-
-	expectedRule := wallarm.ActionBody{
-		ActionID:   actionID,
-		Type:       actionType,
-		AttackType: attackType,
-		RegexID:    regexID,
-		Regex:      regex,
-		Point:      points,
-	}
-
-	var updatedRule *wallarm.ActionBody
-	for _, rule := range *actionHints.Body {
-		if ruleID == rule.ID {
-			updatedRule = &rule
-			break
-		}
-
-		// The response has a different structure so we have to align them
-		// to uniform view then to compare.
-		alignedPoints := alignPointScheme(rule.Point)
-
-		actualRule := &wallarm.ActionBody{
-			ActionID:   rule.ActionID,
-			Type:       rule.Type,
-			AttackType: rule.AttackType,
-			RegexID:    rule.RegexID,
-			Regex:      rule.Regex,
-			Point:      alignedPoints,
-		}
-
-		if cmp.Equal(expectedRule, *actualRule) && equalWithoutOrder(action, rule.Action) {
-			updatedRule = &rule
-			break
-		}
-	}
-
-	if updatedRule == nil {
-		d.SetId("")
-		return nil
-	}
-
-	d.Set("rule_id", updatedRule.ID)
-	d.Set("client_id", clientID)
-	d.Set("active", updatedRule.Active)
-	d.Set("title", updatedRule.Title)
-	d.Set("mitigation", updatedRule.Mitigation)
-	d.Set("set", updatedRule.Set)
-
-	if actionsSet.Len() != 0 {
-		d.Set("action", &actionsSet)
-	} else {
-		log.Printf("[WARN] action was empty so it either doesn't exist or it is a default branch which has no conditions. Actions: %v", &actionsSet)
-	}
-
-	return nil
 }
 
 func resourceWallarmRegexDelete(d *schema.ResourceData, m interface{}) error {
