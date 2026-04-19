18dd44491eab738e4d0f72d8e15a3864a8aa92f8
Dmitry Iovev <iovlev.d@gmail.com>
2026-04-20 00:49:38 +0700

    move isNotFoundError tests to integration_helpers_test.go

--- wallarm/provider/action_helpers_test.go
+++ wallarm/provider/action_helpers_test.go
@@ -1,7 +1,6 @@
 package wallarm
 
 import (
-	"fmt"
 	"testing"
 
 	wallarm "github.com/wallarm/wallarm-go"
@@ -102,26 +101,5 @@ func TestImportAsExistsError(t *testing.T) {
 	msg := err.Error()
 	if msg == "" {
 		t.Error("expected non-empty error message")
-	}
-}
-
-func TestIsNotFoundError_404(t *testing.T) {
-	err := &wallarm.APIError{StatusCode: 404, Body: "not found"}
-	if !isNotFoundError(err) {
-		t.Error("expected true for 404")
-	}
-}
-
-func TestIsNotFoundError_500(t *testing.T) {
-	err := &wallarm.APIError{StatusCode: 500, Body: "server error"}
-	if isNotFoundError(err) {
-		t.Error("expected false for 500")
-	}
-}
-
-func TestIsNotFoundError_NonAPI(t *testing.T) {
-	err := fmt.Errorf("some other error")
-	if isNotFoundError(err) {
-		t.Error("expected false for non-API error")
 	}
 }
