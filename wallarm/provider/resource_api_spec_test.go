ffebaba1bb690835fff9b785f844cce55374a795
Dmitry Iovev <iovlev.d@gmail.com>
2026-04-22 19:01:28 +0700

    api_spec: acceptance tests for Update, Import, AuthHeaders lifecycle

--- wallarm/provider/resource_api_spec_test.go
+++ wallarm/provider/resource_api_spec_test.go
@@ -74,6 +74,131 @@ resource "wallarm_api_spec" %[1]q {
 }`, resourceID, clientID)
 }
 
+func TestAccWallarmAPISpec_Update(t *testing.T) {
+	rnd := generateRandomResourceName(5)
+	resourceName := "wallarm_api_spec." + rnd
+	clientID := os.Getenv("WALLARM_API_CLIENT_ID")
+
+	var firstID string
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheckAPISpec(t) },
+		ProtoV5ProviderFactories: testAccProtoV5ProviderFactories,
+		CheckDestroy:             testAccCheckWallarmAPISpecDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccWallarmAPISpecBasic(rnd, clientID),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "title", "tf-test-"+rnd),
+					func(s *terraform.State) error {
+						firstID = s.RootModule().Resources[resourceName].Primary.Attributes["api_spec_id"]
+						return nil
+					},
+				),
+			},
+			{
+				Config: testAccWallarmAPISpecUpdated(rnd, clientID),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "title", "tf-test-"+rnd+"-updated"),
+					resource.TestCheckResourceAttr(resourceName, "description", "Updated description"),
+					func(s *terraform.State) error {
+						newID := s.RootModule().Resources[resourceName].Primary.Attributes["api_spec_id"]
+						if newID != firstID {
+							return fmt.Errorf("expected api_spec_id to remain %s across Update, got %s (destroy/recreate instead of in-place Update)", firstID, newID)
+						}
+						return nil
+					},
+				),
+			},
+		},
+	})
+}
+
+func testAccWallarmAPISpecUpdated(resourceID, clientID string) string {
+	return fmt.Sprintf(`
+resource "wallarm_api_spec" %[1]q {
+  client_id           = %[2]s
+  title               = "tf-test-%[1]s-updated"
+  description         = "Updated description"
+  file_remote_url     = "https://raw.githubusercontent.com/concentrator/petstore/refs/heads/main/spec.yaml"
+  regular_file_update = false
+  api_detection       = false
+  domains             = ["petstore-tf-test-%[1]s.example.com"]
+  instances           = [1]
+}`, resourceID, clientID)
+}
+
+func TestAccWallarmAPISpec_Import(t *testing.T) {
+	rnd := generateRandomResourceName(5)
+	resourceName := "wallarm_api_spec." + rnd
+	clientID := os.Getenv("WALLARM_API_CLIENT_ID")
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheckAPISpec(t) },
+		ProtoV5ProviderFactories: testAccProtoV5ProviderFactories,
+		CheckDestroy:             testAccCheckWallarmAPISpecDestroy,
+		Steps: []resource.TestStep{
+			{Config: testAccWallarmAPISpecBasic(rnd, clientID)},
+			{
+				ResourceName:            resourceName,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"file.0.signed_url"},
+			},
+		},
+	})
+}
+
+func TestAccWallarmAPISpec_AuthHeaders(t *testing.T) {
+	rnd := generateRandomResourceName(5)
+	resourceName := "wallarm_api_spec." + rnd
+	clientID := os.Getenv("WALLARM_API_CLIENT_ID")
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheckAPISpec(t) },
+		ProtoV5ProviderFactories: testAccProtoV5ProviderFactories,
+		CheckDestroy:             testAccCheckWallarmAPISpecDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccWallarmAPISpecWithAuthHeader(rnd, clientID, "X-Token", "first-value"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "auth_headers.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "auth_headers.0.key", "X-Token"),
+					resource.TestCheckResourceAttr(resourceName, "auth_headers.0.value", "first-value"),
+				),
+			},
+			{
+				Config: testAccWallarmAPISpecWithAuthHeader(rnd, clientID, "X-Token", "second-value"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "auth_headers.0.value", "second-value"),
+				),
+			},
+			{
+				Config: testAccWallarmAPISpecBasic(rnd, clientID),
+				Check:  resource.TestCheckResourceAttr(resourceName, "auth_headers.#", "0"),
+			},
+		},
+	})
+}
+
+func testAccWallarmAPISpecWithAuthHeader(resourceID, clientID, key, value string) string {
+	return fmt.Sprintf(`
+resource "wallarm_api_spec" %[1]q {
+  client_id           = %[2]s
+  title               = "tf-test-%[1]s"
+  description         = "Auth headers test"
+  file_remote_url     = "https://raw.githubusercontent.com/concentrator/petstore/refs/heads/main/spec.yaml"
+  regular_file_update = false
+  api_detection       = false
+  domains             = ["petstore-tf-test-%[1]s.example.com"]
+  instances           = [1]
+  auth_headers {
+    key   = %[3]q
+    value = %[4]q
+  }
+}`, resourceID, clientID, key, value)
+}
+
 func testAccCheckWallarmAPISpecDestroy(s *terraform.State) error {
 	api, err := testAccNewAPIClient()
 	if err != nil {
